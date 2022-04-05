c158c1de658ed573e751b8ed4dffc0f0f07f9249
Apollo11-hub <Antonio-Pollo@outlook.it>
2022-04-05 16:23:03 +0200

    Svolgimento rapido esercizio, prove con css, alert ecc...

---
+++ script.js
@@ -0,0 +1,31 @@
+/*
+1 Chiedere Dati all'utente
+2 Creare Password poco sicura
+
+1 Chiedere Nome
+  Chiedere Cognome
+  Chiedere Colore Preferito
+  Chiedere Data di nascita
+  Sommare Risposte in un unica riga di testo
+
+*/
+
+
+
+const  askName = prompt("Ciao! Qual'è il tuo nome?");
+const  askSurname = prompt ("Che nome di merda :C Vediamo il tuo cognome!?");
+const askColor = prompt ("Di male in peggio...ma posso capirti,tranquillo! Ora tocca al tuo colore preferito... che sarebbe?");
+const  askBirthday = prompt ("In che giorno ti hanno ca...Messo al mondo?");
+
+const generatedPassword = askName + askSurname + askColor + askBirthday ;
+
+const genText =
+
+`
+<p> La tua password: ${generatedPassword}
+
+`
+
+alert("MI DISPIACE PER LA TUA SICUREZZA");
+
+document.getElementById("password").innerHTML = genText;
